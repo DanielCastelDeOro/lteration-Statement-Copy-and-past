cc85058ebb76f7b1002c44e90151b9337f7b0c34
Daniel Castel De Oro <83740100+DanielCastelDeOro@users.noreply.github.com>
2021-06-13 11:40:09 -0600

    Add project files.

---
+++ lteration Statement Copy and past/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+namespace lteration_Statement_Copy_and_past
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new List<int>();
+            int num = 0;
+
+            do
+            {
+                num++;
+                numbers.Add(num);
+            } while (num < 100);
+
+            while (num<200)
+            {
+                num++;
+                numbers.Add(num);
+            }
+
+            Console.WriteLine("Increase:");
+
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Decrease:");
+
+            for (int i = 199; i <= numbers.Count && i >= 0; i--)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
